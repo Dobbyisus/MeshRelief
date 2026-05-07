4355746d435d459d8ff84e82437f4a07b98b3471
Dobbyisus <shashwattiwari884@gmail.com>
2026-05-07 19:38:54 +0530

    resolving issue with github push

--- app/src/main/java/com/meshrelief/core/p2p/PeerDiscovery.java
+++ app/src/main/java/com/meshrelief/core/p2p/PeerDiscovery.java
@@ -3,6 +3,7 @@ package com.meshrelief.core.p2p;
 import java.util.List;
 
 public interface PeerDiscovery {
+    //checking issue with github pusht
     void startDiscovery();
     void stopDiscovery();
     List<Peer> getDiscoveredPeers();
